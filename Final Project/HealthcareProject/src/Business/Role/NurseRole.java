26d5639f1d5da5e27831f647779a203ad4986aa8
monali patel <patel.mona@husky.neu.edu>
2020-04-10 00:10:43 -0400

    work request added

--- Final Project/HealthcareProject/src/Business/Role/NurseRole.java
+++ Final Project/HealthcareProject/src/Business/Role/NurseRole.java
@@ -8,18 +8,20 @@ package Business.Role;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
-import Business.Organization.BloodBankOrganization;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
-import javax.swing.JPanel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author monal
  */
-public class BloodSupplierRole extends Role{
-    public JPanel createWorkArea(JPanel container, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) 
-    {
-      return null;   }
+public class NurseRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+            
+         return null;
+ }
+    
 }
