0e98e04329bc16a0660bfec1c5a6a76f273ef8f3
monali patel <patel.mona@husky.neu.edu>
2020-04-11 21:01:09 -0400

    login done

--- Final Project/HealthcareProject/src/Business/ConfigureASystem.java
+++ Final Project/HealthcareProject/src/Business/ConfigureASystem.java
@@ -21,7 +21,7 @@ public class ConfigureASystem {
         //create user account
         
         
-        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+        Employee employee = system.getEmployeeDirectory().createEmployee("RRH",1);
         
         UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
         
