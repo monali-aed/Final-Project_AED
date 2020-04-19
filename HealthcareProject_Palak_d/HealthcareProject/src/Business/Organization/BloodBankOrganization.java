9871cd2b16a35424c735c4e16d3ad4fb671a1b67
Palak Maal <maal.p@husky.neu.edu>
2020-04-19 03:43:10 -0400

    FinalProject_palak_d

---
+++ HealthcareProject_Palak_d/HealthcareProject/src/Business/Organization/BloodBankOrganization.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+import Business.Role.BloodSupplierRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author monal
+ */
+public class BloodBankOrganization {
+    BloodBankOrganization() {
+        super();
+    }
+
+    
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new BloodSupplierRole());
+        return roles;
+    }
+
+    public String getType() {
+    return "BloodBank Organization";
+    }
+}
