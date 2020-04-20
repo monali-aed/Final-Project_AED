d5168e61c24fb17eff7b597eb5ffa27fb34e9624
Palak Maal <maal.p@husky.neu.edu>
2020-04-20 03:33:06 -0400

    FinalProject_palak_h

---
+++ HealthcareProject_g/src/Business/Inventory/MedicineDirectory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Inventory;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author monal
+ */
+public class MedicineDirectory {
+      private ArrayList<Medicine> medicineList;
+
+    public MedicineDirectory() 
+    {
+        medicineList=new ArrayList<Medicine>();
+    }
+
+    public ArrayList<Medicine> getMedicineList() 
+    {
+        return medicineList;
+    }
+    public Medicine add()
+    {
+        Medicine medicine=new Medicine();
+        medicineList.add(medicine);
+        return medicine;
+    }
+}
