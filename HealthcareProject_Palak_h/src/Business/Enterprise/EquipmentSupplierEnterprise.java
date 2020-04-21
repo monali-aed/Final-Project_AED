a51eea3a3770176b57b36566488ffe9125bb7a17
Palak Maal <maal.p@husky.neu.edu>
2020-04-21 01:39:33 -0400

    FinalProject_palak_h

---
+++ HealthcareProject_Palak_h/src/Business/Enterprise/EquipmentSupplierEnterprise.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Enterprise.Enterprise.EnterpriseType;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author monal
+ */
+public class EquipmentSupplierEnterprise {
+   
+        public EquipmentSupplierEnterprise(String name)
+    {
+        super();
+    }
+        public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
